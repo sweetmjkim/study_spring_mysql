eadf2cca32dc068b95c733e9041aee96e2ea0d43
sweetmjkim <sweet.mjkim@gmail.com>
2024-04-17 03:19:27 +0000

    add

--- src/main/java/com/example/co_templates/services/CommonCodeService.java
+++ src/main/java/com/example/co_templates/services/CommonCodeService.java
@@ -1,8 +1,8 @@
 package com.example.co_templates.services;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -31,14 +31,15 @@ public class CommonCodeService {
 
     public Object selectTotal(Map dataMap) {
         String sqlMapId = "CommonCode.selectTotal";
+
         Object result = shareDao.getOne(sqlMapId, dataMap);
         return result;
-    }
-    
+    }    
+
     public Map selectSearchWithPagination(Map dataMap) {
         // 페이지 형성 위한 계산
         int totalCount = (int) this.selectTotal(dataMap);
-
+        
         int currentPage = 1;
         if(dataMap.get("currentPage") != null) {
             currentPage = Integer.parseInt((String)dataMap.get("currentPage"));    // from client in param
@@ -52,22 +53,52 @@ public class CommonCodeService {
         String sqlMapId = "CommonCode.selectSearchWithPagination";
         dataMap.put("pageScale", paginations.getPageScale());
         dataMap.put("pageBegin", paginations.getPageBegin());
-
+        
         result.put("resultList", shareDao.getList(sqlMapId, dataMap)); // 표현된 레코드 정보
         return result;
     }
 
-    public void callDao(HashMap<String, Object> dataMap){
-        // 여러개 가져오기
-        String sqlMapId = "CommonCode.selectBysearch";
-        Object list = shareDao.getList(sqlMapId, dataMap);
-    
+    public Object deleteWithIn(Map dataMap){
+        String sqlMapId = "CommonCode.deletewithin";
+        Object count = shareDao.delete(sqlMapId, dataMap);
+        return count;
+    }
+
+    public Map selectSearchWithPaginationAndDeletes(Map dataMap) {
+        // delete
+        if (dataMap.get("deleteIds") != null){
+            Object count = this.deleteWithIn(dataMap);
+        }
+
+        // 페이지 형성 위한 계산
+        int totalCount = (int) this.selectTotal(dataMap);
+        
+        int currentPage = 1;
+        if(dataMap.get("currentPage") != null) {
+            currentPage = Integer.parseInt((String)dataMap.get("currentPage"));    // from client in param
+        }
+
+        Paginations paginations = new Paginations(totalCount, currentPage);
+        HashMap result = new HashMap<>();
+        result.put("paginations", paginations); // 페이지에 대한 정보
+
+        // page record 수
+        String sqlMapId = "CommonCode.selectSearchWithPagination";
+        dataMap.put("pageScale", paginations.getPageScale());
+        dataMap.put("pageBegin", paginations.getPageBegin());
+        
+        result.put("resultList", shareDao.getList(sqlMapId, dataMap)); // 표현된 레코드 정보
+        return result;
+    }
+
+    public void callDao(HashMap<String, Object> dataMap) {
+
+        this.selectMany(dataMap);
         // 한개만 가져오기
-        sqlMapId = "CommonCode.selectByUID";
+        String sqlMapId = "CommonCode.selectByUID";
         dataMap.put("PK_UNIQUE", "CC002");
         Object one = shareDao.getOne(sqlMapId, dataMap);
 
-        // 입력
         sqlMapId = "CommonCode.insert";
         // (`PK_UNIQUE`, `CODE_NAME`, `DESCRIPTION`, `PARENT_UNIQUE`)
         // VALUES
@@ -79,35 +110,33 @@ public class CommonCodeService {
         dataMap.put("PARENT_UNIQUE", "");
         Object insert = shareDao.insert(sqlMapId, dataMap);
 
-        // 수정
         sqlMapId = "CommonCode.update";
         dataMap.put("PK_UNIQUE", pk_unique);
         dataMap.put("CODE_NAME", "test_ROOT");
         Object update = shareDao.update(sqlMapId, dataMap);
 
-        // 삭제
         sqlMapId = "CommonCode.delete";
         dataMap.put("PK_UNIQUE", pk_unique);
         Object delete = shareDao.delete(sqlMapId, dataMap);
         return;
     }
 
-    public HashMap<String, Object> mixed(Integer pageNumber, Integer pkid){
+    public HashMap<String, Object> mixed(Integer pageNumber, Integer pkid) {
         HashMap<String, Object> resultMap = new HashMap<>();
         resultMap.put("list", this.list(pageNumber));
         resultMap.put("view", this.view(pkid));
         return resultMap;
     }
 
-    public ArrayList<HashMap<String, Object>> list(Integer pageNumber){
+    public ArrayList<HashMap<String, Object>> list(Integer pageNumber) {
         ArrayList<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
- 
+
         HashMap<String, Object> item = new HashMap<>();
         item.put("PK_ID", 1);
         item.put("FK_ID", 10);
         item.put("NAME", "Excalibur");
         itemList.add(item);
-    
+
         item = new HashMap<>();
         item.put("PK_ID", 2);
         item.put("FK_ID", 20);
@@ -117,7 +146,7 @@ public class CommonCodeService {
         return itemList;
     }
 
-    public HashMap<String, Object> view(Integer pkId){
+    public HashMap<String, Object> view(Integer pkId) {
         HashMap<String, Object> itemDetails = new HashMap<>();
 
         String fkId = "FK_0382";
